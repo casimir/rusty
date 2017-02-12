cb1e64876fe1d054edd2e9ef59fb4609dbabfd66
Martin Chaine <chaine.martin@gmail.com>
2017-02-12 01:48:26 +0100

    rework gui

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,8 +1,12 @@
+extern crate image;
 extern crate sdl2;
 
-use sdl2::pixels::Color::RGB;
-use sdl2::render::Renderer;
-use sdl2::rect::Point;
+use self::sdl2::event::Event;
+use self::sdl2::keyboard::Keycode;
+use self::sdl2::pixels::Color::RGB;
+use self::sdl2::render::Renderer;
+use self::sdl2::rect::Point;
+use std::time::Duration;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Color {
@@ -19,74 +23,140 @@ pub enum Pixel {
 }
 
 pub struct Canvas {
-    pub w: usize,
-    pub h: usize,
+    pub width: u32,
+    pub height: u32,
     pixels: Vec<Pixel>,
+    dirty: bool,
 }
 
 impl Canvas {
-    fn get(&self, x: usize, y: usize) -> Pixel {
-        if x > self.w || y > self.h {
-            error!("Invalid coordinates: ({}, {})", x, y);
+    fn get(&self, x: u32, y: u32) -> Pixel {
+        if x > self.width || y > self.height {
+            error!("invalid coordinates: ({}, {})", x, y);
             Pixel::Blank
         } else {
-            self.pixels[x + y * self.w]
+            let index = (x + y * self.width) as usize;
+            self.pixels[index]
         }
     }
 
-    pub fn set(&mut self, x: usize, y: usize, c: Color) {
-        if x > self.w || y > self.h {
-            error!("Invalid coordinates: ({}, {})", x, y);
+    pub fn set(&mut self, x: u32, y: u32, c: Color) {
+        if x > self.width || y > self.height {
+            error!("invalid coordinates: ({}, {})", x, y);
         } else {
-            self.pixels[x + y * self.w] = Pixel::Data(c)
+            let index = (x + y * self.width) as usize;
+            self.pixels[index] = Pixel::Data(c);
+            self.dirty = true;
         }
     }
 }
 
-pub struct Backend {
+#[derive(Debug)]
+pub enum Error {
+    Creation(sdl2::video::WindowBuildError),
+    InvalidData(sdl2::IntegerOrSdlError),
+    Export(::std::io::Error),
+}
+
+impl From<sdl2::video::WindowBuildError> for Error {
+    fn from(val: sdl2::video::WindowBuildError) -> Error {
+        Error::Creation(val)
+    }
+}
+
+impl From<sdl2::IntegerOrSdlError> for Error {
+    fn from(val: sdl2::IntegerOrSdlError) -> Error {
+        Error::InvalidData(val)
+    }
+}
+
+impl From<::std::io::Error> for Error {
+    fn from(val: ::std::io::Error) -> Error {
+        Error::Export(val)
+    }
+}
+
+pub struct Context {
     pub canvas: Canvas,
     pub context: sdl2::Sdl,
     renderer: Renderer<'static>,
 }
 
-impl Backend {
-    pub fn new(x: u32, y: u32) -> Backend {
+impl Context {
+    pub fn new(width: u32, height: u32) -> Result<Context, Error> {
         let sdl_context = sdl2::init().unwrap();
         let video = sdl_context.video().unwrap();
-        let window = video.window("Rusty", x, y)
+        let window = video.window("Rusty", width, height)
             .position_centered()
             .opengl()
-            .build()
-            .unwrap();
-        let renderer = window.renderer().build().unwrap();
+            .build()?;
+        let renderer = window.renderer().build()?;
 
-        let xs = x as usize;
-        let ys = y as usize;
-        Backend {
+        Ok(Context {
             context: sdl_context,
             renderer: renderer,
             canvas: Canvas {
-                w: xs,
-                h: ys,
-                pixels: vec![Pixel::Blank; xs * ys],
+                width: width,
+                height: height,
+                pixels: vec![Pixel::Blank; (width * height) as usize],
+                dirty: false,
             },
-        }
+        })
     }
 
     pub fn paint(&mut self) {
-        let ref mut rend = self.renderer;
-        for x in 0..self.canvas.w {
-            for y in 0..self.canvas.h {
-                match self.canvas.get(x, y) {
-                    Pixel::Data(color) => {
-                        rend.set_draw_color(RGB(color.r, color.g, color.b));
-                        rend.draw_point(Point::new(x as i32, y as i32));
-                    }
-                    Pixel::Blank => {}
-                };
+        if self.canvas.dirty {
+            let ref mut rend = self.renderer;
+            for x in 0..self.canvas.width {
+                for y in 0..self.canvas.height {
+                    match self.canvas.get(x, y) {
+                        Pixel::Data(color) => {
+                            rend.set_draw_color(RGB(color.r, color.g, color.b));
+                            rend.draw_point(Point::new(x as i32, y as i32));
+                        }
+                        Pixel::Blank => {}
+                    };
+                }
             }
+            self.canvas.dirty = false;
+            rend.present();
+            // XXX show metrics
         }
-        rend.present();
-        // XXX show metrics
+    }
+
+    pub fn export(&self) -> Result<String, Error> {
+        use self::image::{ImageBuffer, Rgba};
+        let img = ImageBuffer::from_fn(self.canvas.width,
+                                       self.canvas.height,
+                                       |x, y| match self.canvas.get(x, y) {
+                                           Pixel::Data(color) => {
+                                               Rgba { data: [color.r, color.g, color.b, color.a] }
+                                           }
+                                           Pixel::Blank => Rgba { data: [0, 0, 0, 0] },
+                                       });
+        let filename = "export.png";
+        img.save(filename)?;
+        println!("exported as: {}", filename);
+        Ok(filename.into())
+    }
+
+    pub fn run(&mut self) -> Result<(), Error> {
+        let mut event_pump = self.context.event_pump().unwrap();
+        'running: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. } |
+                    Event::KeyDown { keycode: Some(Keycode::Q), .. } => break 'running,
+                    Event::KeyDown { keycode: Some(Keycode::E), .. } => {
+                        self.export().unwrap();
+                    }
+                    _ => {}
+                }
+            }
+
+            self.paint();
+            ::std::thread::sleep(Duration::from_millis(17));
+        }
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,34 @@
-extern crate clock_ticks;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
-extern crate sdl2;
 
 mod graphics;
+mod math;
 
 use graphics::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
 
 pub fn main() {
     env_logger::init().unwrap();
-    let fps = 30;
-    let delta: u64 = 1000000000 / fps;
 
-    let mut gui = graphics::Backend::new(800, 600);
-    let mut curr_time = clock_ticks::precise_time_ns();
-    let mut last_time = curr_time;
-    let mut running = true;
-
-    colormap(&mut gui);
-    let mut event_pump = gui.context.event_pump().unwrap();
-    while running {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Q), .. } => running = false,
-                Event::KeyDown { keycode: Some(Keycode::E), .. } => {
-                    println!("Should export to JPEG");
-                }
-                _ => {}
-            }
-        }
-
-        // The rest of the game loop goes here...
-
-        if curr_time - last_time > delta {
-            gui.paint();
-            last_time = clock_ticks::precise_time_ns();
-        }
-        curr_time = clock_ticks::precise_time_ns();
-    }
+    let mut gui = graphics::Context::new(800, 600).unwrap();
+    colormap(&mut gui.canvas);
+    gui.run().unwrap();
 }
 
-fn colormap(gui: &mut graphics::Backend) {
-    let h = gui.canvas.h;
-    let w = gui.canvas.w;
-    for y in 0..gui.canvas.h {
-        for x in 0..gui.canvas.w {
-            gui.canvas.set(x,
-                           y,
-                           Color {
-                               r: (x as f64 / w as f64 * 255.0) as u8,
-                               g: (y as f64 / h as f64 * 255.0) as u8,
-                               b: (((x * y) as f64 / (h.pow(2) + w.pow(2)) as f64).sqrt() *
-                                   255.0) as u8,
-                               a: 255,
-                           })
+fn colormap(canvas: &mut graphics::Canvas) {
+    let h = canvas.height;
+    let w = canvas.width;
+    for y in 0..canvas.height {
+        for x in 0..canvas.width {
+            canvas.set(x,
+                       y,
+                       Color {
+                           r: (x as f64 / w as f64 * 255.0) as u8,
+                           g: (y as f64 / h as f64 * 255.0) as u8,
+                           b: (((x * y) as f64 / (h.pow(2) + w.pow(2)) as f64).sqrt() * 255.0) as
+                              u8,
+                           a: 255,
+                       })
         }
     }
-    gui.paint();
 }
